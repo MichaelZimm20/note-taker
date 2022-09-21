c46466b4a404f6eda7befa9c515d5f7e14a19b8e
DJMIKE20 <m.zimm20@gmail.com>
2022-09-21 14:30:06 -0400

    initial setup for server

---
+++ server.js
@@ -0,0 +1,15 @@
+// express package 
+const express = require('express');
+
+// sets environment for Heroku, if not use default port 3001
+const PORT = process.env.PORT || 3001;
+
+// instantiate/creating the server 
+const app = express();
+
+
+
+//express to listen
+app.listen(3001, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
