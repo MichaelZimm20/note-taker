e1c80c87de9114f5f4ff72e3d39d5d9a1096a058
DJMIKE20 <m.zimm20@gmail.com>
2022-09-22 11:19:34 -0400

    refactor noteTakerRoutes to create unique id and customize object output

--- routes/apiRoutes/noteTakerRoutes.js
+++ routes/apiRoutes/noteTakerRoutes.js
@@ -1,11 +1,14 @@
 // define router: allows us to declare routes in any file as long as using the proper middleware. Can not use "app" because server.js is using it
 const router = require('express').Router();
 
-// importing db.json()
+// importing db.json() 
 const db = require('../../db/db.json'); 
 
 // import note Functions from buildNote
 const { createNote, deleteNote } = require('../../lib/buildNote');
+
+// import unqiue ID 
+const uniqid = require('uniqid'); 
 
 // get notes and read db.json and returns all saved notes as json
 router.get('/notes', (req, res) => {
@@ -16,8 +19,15 @@ router.get('/notes', (req, res) => {
 
 // receives a new note to save on request body, and add to json file
 router.post('/notes', (req, res) => {
-    req.body.id = db.length.toString();
-    let newNote = createNote(req.body, db);
+    
+    // TODO: CREATE unique id. ask in office hours
+    const noteToAdd = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uniqid(),
+   }
+    // req.body.id = db.length.toString();
+    let newNote = createNote(noteToAdd, db);
     res.json(newNote);
 })
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // express package 
 const express = require('express');
+const apiRoutes =  require('./routes/apiRoutes');
 
 // sets environment for Heroku, if not use default port 3001
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,12 @@ const PORT = process.env.PORT || 3001;
 // instantiate/creating the server 
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
 
 
 //express to listen and start server
